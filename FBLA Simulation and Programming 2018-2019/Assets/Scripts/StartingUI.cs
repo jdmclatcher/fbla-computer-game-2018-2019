79027547853773dea07dcd6997db45d82790d874
bananananer joe <32718406+bananananer-joe@users.noreply.github.com>
2019-02-06 18:31:20 -0500

    Revert "added more animations to the info UI"
    This reverts commit 175f3c109fe693d76a6b03df2fa6fa7f1a825a6f.

--- FBLA Simulation and Programming 2018-2019/Assets/Scripts/StartingUI.cs
+++ FBLA Simulation and Programming 2018-2019/Assets/Scripts/StartingUI.cs
@@ -1,21 +1,12 @@
 ﻿using UnityEngine.SceneManagement;
-using System.Collections;
 using UnityEngine;
 
 public class StartingUI : MonoBehaviour {
 
-    [SerializeField] private GameObject curtainClose;
-
-    private void Start()
-    {
-        curtainClose.SetActive(false); // disable by default
-    }
 
     public void PlayGame()
     {
-        // run routine
-        StartCoroutine(ILoadScene("World"));
-        // SceneManager.LoadScene("World");
+        SceneManager.LoadScene("World");
     }
 
     public void QuitGame()
@@ -25,23 +16,7 @@ public class StartingUI : MonoBehaviour {
 
     public void LoadHowTo()
     {
-        // run routine
-        StartCoroutine(ILoadScene("Info"));
-        // SceneManager.LoadScene("Info");
-    }
-
-    IEnumerator ILoadScene(string sceneToLoad)
-    {
-        curtainClose.SetActive(true); // set curtain active
-        // get ref to anim on curtain
-        Animation curtainAnim = curtainClose.GetComponent<Animation>();
-        curtainAnim.Play("Curtain Close"); // play anim
-
-        // wait until anim is done playing
-        yield return new WaitUntil(() => curtainAnim.IsPlaying("Curtain Close") == false);
-
-        // then leave scene
-        SceneManager.LoadScene(sceneToLoad); // loads desired scene
+        SceneManager.LoadScene("Info");
     }
 	
 }
